edd53f0d5b9b86c46a80f12a8cd19c258872c3c8
Rodrigo Almeida <rodrigoalmeida08@hotmail.com>
2024-03-21 20:59:41 +0000

    dummy data showing in a table format

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express');
+const fs = require('fs').promises;
 const app = express();
 const port = 4200;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-const data = [
-    { name: 'Item 1', value: 'This is item 1' },
-    { name: 'Item 2', value: 'This is item 2' }
-];
 
-// Define a route to render the index.ejs template
-app.get('/dataset', (req, res) => {
-    res.render('index', { data: data });
+app.get('/data', async (req, res) => {
+    const { limit = 10 } = req.query;
+    try {
+        res.set({
+            'Cache-Control': 'no-cache, no-store, must-revalidate', // Prevent caching of any kind
+            'Pragma': 'no-cache', // HTTP 1.0.
+            'Expires': '0' // Proxies.
+        });
+        const dataText = await fs.readFile('test_data.txt', 'utf8');
+        const data = JSON.parse(dataText).slice(0, Number(limit));
+        res.render('dataPage', { data, limit: Number(limit) });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error reading data file');
+    }
 });
 
-
-// Define a root route if necessary
-app.get('/', (req, res) => {
-    res.render('someOtherTemplate');
-    
-});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
