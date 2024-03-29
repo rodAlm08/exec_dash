a862205409296e25c8f465a7916060baee400e7c
Rodrigo Almeida <rodrigoalmeida08@hotmail.com>
2024-03-29 18:27:47 +0000

    front end modified to show mock image generated by neural network

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
+app.use('/images', express.static('C:/Users/rodri/repos/exec_dash/images'));
+
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
