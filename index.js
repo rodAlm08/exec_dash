63da4715d674f3e8c23e851c82bac83a3b69edb5
Rodrigo Almeida <rodrigoalmeida08@hotmail.com>
2024-04-09 19:58:53 +0100

    pagination added

--- index.js
+++ index.js
@@ -1,254 +1,177 @@
-require('dotenv').config({ path: './.env' });
+require("dotenv").config({ path: "./.env" });
 
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 const app = express();
 const port = 4200;
 
-
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
-app.use('/images', express.static('C:/Users/rodri/repos/exec_dash/images'));
-app.set('view engine', 'ejs');
+app.use(express.json());
+app.use("/images", express.static("C:/Users/rodri/repos/exec_dash/images"));
+app.set("view engine", "ejs");
 
 exports.removeColumn = (matrix, columnName) => {
-    matrix.forEach(row => {
-        delete row[columnName];
-    });
-    return matrix;
-}
+  matrix.forEach((row) => {
+    delete row[columnName];
+  });
+  return matrix;
+};
 
 async function fetchData(apiUrl, headers) {
-    const response = await axios.get(apiUrl, { headers });
-    return response.data;
+  const response = await axios.get(apiUrl, { headers });
+  return response.data;
 }
-
-// function constructQueryParamsString(query) {
-//     return Object.keys(query)
-//         .map(key => {
-//             if (Array.isArray(query[key])) {
-//                 return query[key].map(k => `${key}=${k}`).join('&');
-//             } else {
-//                 return `${key}=${query[key]}`
-//             }
-//         })
-//         .join('&');
-// }
 
 function constructQueryParamsString(query) {
-    return Object.keys(query)
-        .map(key => {
-            if (Array.isArray(query[key])) {
-                return query[key].map(k => `${encodeURIComponent(key)}=${encodeURIComponent(k)}`).join('&');
-            } else {
-                return `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`;
-            }
-        })
-        .join('&');
+  return Object.keys(query)
+    .map((key) => {
+      if (Array.isArray(query[key])) {
+        return query[key]
+          .map((k) => `${encodeURIComponent(key)}=${encodeURIComponent(k)}`)
+          .join("&");
+      } else {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`;
+      }
+    })
+    .join("&");
 }
-
 
 function determineSelectedColumns(reqQuery, allColumns, excludeColumns) {
-  return reqQuery.columns ? reqQuery.columns.filter(column => !excludeColumns.includes(column)) : allColumns.filter(column => !excludeColumns.includes(column));
+  return reqQuery.columns
+    ? reqQuery.columns.filter((column) => !excludeColumns.includes(column))
+    : allColumns.filter((column) => !excludeColumns.includes(column));
 }
-
 
 async function handleRequest(req, res) {
-    const valuableColumns = [
-        "_id",
-        "_date",
-        "_user",
-        "fm_avg_trk_time",
-        "fm_accuracy",
-        "vx_avg_res_time",
-        "vx_shot_accuracy",
-        "vx_trg_accuracy",
-        "au_avg_res_time",
-        "bm_HR_max",
-        "bm_HR_avg",
-        "bm_HR_var",
-        "bm_act_steps",
-        "bm_sleep",
-    ];
+  const valuableColumns = [
+    "_id",
+    "_date",
+    "_user",
+    "fm_avg_trk_time",
+    "fm_accuracy",
+    "vx_avg_res_time",
+    "vx_shot_accuracy",
+    "vx_trg_accuracy",
+    "au_avg_res_time",
+    "bm_HR_max",
+    "bm_HR_avg",
+    "bm_HR_var",
+    "bm_act_steps",
+    "bm_sleep",
+  ];
 
-    const headers = { 'Authorization': `Bearer ${process.env.API_SECRET_KEY}` };
+  const baseEndpoint = "http://54.236.53.46:4000/api/dataset";
+  const headers = { Authorization: `Bearer ${process.env.API_SECRET_KEY}` };
 
-  //  try {
-        const queryParams = Object.keys(req.query).length === 0 ? valuableColumns : Object.keys(req.query);
-        const excludeColumns = ['_id', '_date', '_user'];
-        const selectedColumns = determineSelectedColumns(req.query, valuableColumns, excludeColumns);
-        var totalCount = 0;
-        //console.log(req);
-        let data;
-        //http://54.236.53.46:4000/
-        if (req.query.submit === 'filter') {
-            data = await fetchData("http://54.236.53.46:4000/api/dataset", headers);
-            //data = await fetchData("http://localhost:3000/api/dataset", headers);
-            totalCount = data.length;
-            console.log('Total count filter:', totalCount);
+  const currentPage = parseInt(req.query.page, 10) || 1;
+  const page = parseInt(req.query.page, 10) || 1;
+  const rows = parseInt(req.query.rows, 10) || 10;
+  const apiUrl = "";
+  const startIndex = (page - 1) * rows;
+  const endIndex = page * rows;
+  let data;
 
-        } else if (req.query.submit === 'clean') {
-            const queryString = constructQueryParamsString(req.query);
-            const apiUrl = `http://54.236.53.46:4000/api/dataset?${queryString}`;
-            //const apiUrl = `http://localhost:3000/api/dataset?${queryString}`;
-            data = await fetchData(apiUrl, headers);
-            totalCount = data.length;
-            console.log('Query String:', queryString);
-            console.log('Total count clean:', totalCount);
+  const queryParams =
+    Object.keys(req.query).length === 0
+      ? valuableColumns
+      : Object.keys(req.query);
+  const excludeColumns = ["_id", "_date", "_user"];
 
-        } else {
-            data = await fetchData("http://54.236.53.46:4000/api/dataset", headers);
-            //data = await fetchData("http://localhost:3000/api/dataset", headers);
-            totalCount = data.length;
-            console.log('Total count none:', totalCount);
-        }
+  const selectedColumns = determineSelectedColumns(
+    req.query,
+    valuableColumns,
+    excludeColumns
+  );
 
-        let filteredData = data;
+  var totalCount = 0;
 
-        const relevantQueryParams = queryParams.filter(param => !['submit', 'rows'].includes(param) && selectedColumns.includes(param));
+   try {
+  //http://54.236.53.46:4000/
+  if (req.query.submit === "filter") {
+    data = await fetchData("http://54.236.53.46:4000/api/dataset", headers);
+    //data = await fetchData("http://localhost:3000/api/dataset", headers);
+    totalCount = data.length;
+  } else if (req.query.submit === "clean") {
+    const queryString = constructQueryParamsString({
+      ...req.query,
+      page: undefined,
+    });
+    const apiUrl = `${baseEndpoint}?${queryString}`;
+    data = await fetchData(apiUrl, headers);
+    totalCount = data.length;
+    //console.log('apiUrl:', apiUrl);
+  } else {
+    data = await fetchData("http://54.236.53.46:4000/api/dataset", headers);
+    //data = await fetchData("http://localhost:3000/api/dataset", headers);
+    totalCount = data.length;
+  }
 
-        relevantQueryParams.forEach(column => {
-            if (!selectedColumns.includes(column)) {
-                filteredData = removeColumn(filteredData, column);
-            }
-        });
+  let filteredData = data.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(totalCount / rows);
 
-        totalCount = filteredData.length;
-        console.log('Total count:', totalCount);
-        
-        res.render('./dataPage', {
-            data: filteredData,
-            totalCount: totalCount,
-            columns: selectedColumns,
-            selectedColumns: selectedColumns,
-            req: req
-        });
+  const relevantQueryParams = queryParams.filter(
+    (param) =>
+      !["submit", "rows"].includes(param) && selectedColumns.includes(param)
+  );
 
- //   } catch (error) {
-   //     res.status(500).send('Failed to fetch data');
-    //}
+  relevantQueryParams.forEach((column) => {
+    if (!selectedColumns.includes(column)) {
+      filteredData = removeColumn(filteredData, column);
+    }
+  });
+
+  const paginationLinks = generatePaginationLinks(currentPage, totalPages, req.query);
+  
+  res.render("./dataPage", {
+    data: filteredData,
+    totalCount: totalCount,   
+    rowsPerPage: rows,
+    apiUrl: apiUrl,
+    columns: selectedColumns,
+    selectedColumns: selectedColumns,
+    paginationLinks: paginationLinks,
+    req: req,
+  });
+    } catch (error) {
+       res.status(500).send("Failed to fetch data");
+    }
 }
 
+function generatePaginationLinks(currentPage, totalPages, originalQuery) {
+    let paginationLinks = [];
+  
+    for (let i = 1; i <= totalPages; i++) {
+      // Clone the original query parameters and update the page number
+      let queryParams = { ...originalQuery, page: i };
+      
+      // Construct the query string for the current page link
+      let queryString = Object.keys(queryParams).map(key => {
+        return Array.isArray(queryParams[key])
+          ? queryParams[key].map(value => `${encodeURIComponent(key)}[]=${encodeURIComponent(value)}`).join('&')
+          : `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`;
+      }).join('&');
+  
+      paginationLinks.push({
+        page: i,
+        queryString: queryString,
+        isActive: i === currentPage
+      });
+    }
+  
+    return paginationLinks;
+  }
 
-app.get('/dashboard', async (req, res) => {
-    await handleRequest(req, res);
-    //console.log('Dashboard', req.query);
+app.get("/dashboard", async (req, res) => {
+  await handleRequest(req, res);
 });
-
-
-// app.get('/dashboard', async (req, res) => {
-
-//     const valuable = [
-//         "_id",
-//         "_date",
-//         "_user",
-//         "fm_avg_trk_time",
-//         "fm_accuracy",
-//         "vx_avg_res_time",
-//         "vx_shot_accuracy",
-//         "vx_trg_accuracy",
-//         "au_avg_res_time",
-//         "bm_HR_max",
-//         "bm_HR_avg",
-//         "bm_HR_var",
-//         "bm_act_steps",
-//         "bm_sleep",
-//     ];
-//     //console.log('precess.env', process.env.API_SECRET_KEY);
-//     // create authorization header
-//      const headers = {'Authorization': `Bearer ${process.env.API_SECRET_KEY}`};
-
-//     try {
-//         const allColumns = valuable;
-//         console.log('All Columns:', allColumns);
-
-//         if(req.query.submit === 'filter'){
-//                 console.log('Queryparams', queryParams);
-           
-//             // Fetch data from the external API
-//             data = await axios.get("https://zerofourtwo.net/api/dataset", {headers});
-
-//             // data = await axios.get("https://zerofourtwo.net/api/dataset");
-             
-//         } else if(req.query.submit === 'clean'){
-//             console.log('submit clean')
-          
-//             const qp = Object.keys(req.query).map(
-//                 key => {
-//                         console.log('key', req.query[key])
-//                         if(Array.isArray(req.query[key])){
-//                             console.log('isArray');
-//                             return req.query[key].map(k => `${key}=${k}`).join('&');
-//                         } else {
-//                             console.log('isNotArray');
-//                             return `${key}=${req.query[key]}`
-//                         }
-//                     }
-//                 ).join('&');
-//             const apiUrl = `https://zerofourtwo.net/api/dataset?${qp}`;
-//             console.log('apiUrl', apiUrl);
-
-//             // Fetch data from the external API
-//             const response = await axios.get(apiUrl, {headers});
-//             data = response;
-//             console.log('data inside clean', data.data);
-//             //data = await axios.get("https://zerofourtwo.net/api/dataset" + queryParams);
-//         }
-//         else{
-//             console.log('submit not filter')
-//             data = await axios.get("https://zerofourtwo.net/api/dataset", {headers});
-//             //console.log('Data LINE 40:', data.data);
-//         }
-
-//         data = data.data;
-
-//         const queryParams = Object.keys(req.query).length === 0 ? allColumns : Object.keys(req.query);        
-
-//         console.log('Query parameters:', queryParams);
-
-//         // Define columns to exclude
-//         const excludeColumns = ['_id', '_date', '_user'];
-
-//         // Determine selected columns based on query parameters, excluding the ones to be ignored
-//         var selectedColumns = req.query.columns ? [].concat(req.query.columns) : allColumns.filter(column => !excludeColumns.includes(column));
-
-//         var filteredData = data;        
-
-//         // Filter out unwanted query parameters (like 'submit', 'rows') and ensure they're not in selectedColumns
-//         const relevantQueryParams = queryParams.filter(param => !['submit', 'rows'].includes(param) && selectedColumns.includes(param));
-
-//         console.log('Selected Columns:', selectedColumns);          
-//         relevantQueryParams.forEach(column => {
-//             if (!selectedColumns.includes(column)) {
-//                 filteredData = removeColumn(filteredData, column);
-//             }
-//         });
-
-//         res.render('./dataPage', {
-//             data: filteredData,
-//             columns: selectedColumns,
-//             selectedColumns:selectedColumns, 
-//             req:req 
-//         });
-       
-        
-//     } catch (error) {
-//       //  console.error('Failed to fetch data:', error);
-//         res.status(500).send('Failed to fetch data');
-//     }
-// });
-
 
 module.exports.constructQueryParamsString = constructQueryParamsString;
 module.exports.determineSelectedColumns = determineSelectedColumns;
 module.exports.fetchData = fetchData;
 
-
-
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
-
